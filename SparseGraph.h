7dfcea429417fa27e156c53e84ab1f93015640c6
Neilai <1131894367@qq.com>
2017-12-19 22:02:00 +0800

    update min path algoritmns in sparsegraph

--- SparseGraph.h
+++ SparseGraph.h
@@ -69,6 +69,155 @@ public:
 		}
 	}
 
+	double Prim(int s,vector<Edge> &tree){		
+		double mindis,len=0;
+		int minindex;
+		int *visited=new int[v];
+		double *dist=new double[v];
+		int *source=new int[v];
+		for(int i=0;i<v;i++){
+			visited[i]=0;
+			dist[i]=0x3f3f3f3f;
+			source[i]=0;
+		}
+		dist[s]=0;		
+		for(int i=0;i<v;i++){	
+			mindis=0x3f3f3f3f;
+			minindex=0;
+			//寻找最小边
+			for(int j=0;j<v;j++)
+				if(!visited[j]&&mindis>=dist[j]){
+					mindis=dist[j];
+					minindex=j;
+				}		
+			visited[minindex]=1;
+			
+			if(!(minindex==0&&source[minindex]==0))
+				tree.push_back(Edge(source[minindex],minindex,-1,mindis));
+			len+=mindis;
+			//取这个顶点后进行松弛
+			adjIterator iter(*this,minindex);
+			for(Edge e=iter.begin();!iter.end();e=iter.next())
+				if(!visited[e.to]&&dist[e.to]>e.weight){
+					dist[e.to]=e.weight;	
+					source[e.to]=e.from;
+				}
+		}
+		return len;
+
+	}	
+
+
+
+	void Dijkstra(int s,int *path,double *dist){		
+		int mindis,minindex;
+		int *visited=new int[v];
+		for(int i=0;i<v;i++){
+			visited[i]=0;
+			path[i]=s;
+			dist[i]=0x3f3f3f3f;
+		}		
+		path[s]=-1;dist[s]=0;
+		for(int i=0;i<v;i++){	
+			mindis=0x3f3f3f3f;
+			minindex=0;
+			//寻找最小边
+			for(int j=0;j<v;j++)
+				if(!visited[j]&&mindis>=dist[j]){
+					mindis=dist[j];
+					minindex=j;
+				}
+			visited[minindex]=1;
+			//取这个顶点后进行松弛
+			adjIterator iter(*this,minindex);
+			for(Edge e=iter.begin();!iter.end();e=iter.next())
+				if(!visited[e.to]&&dist[e.to]>dist[e.from]+e.weight){
+					dist[e.to]=dist[e.from]+e.weight;
+					path[e.to]=e.from;
+				}	
+		}
+
+	}
+
+	void BellmanFord(int s,int *path,double *dis){
+		for(int i=0; i<v;i++){
+			dis[i]=0x3f3f3f3f;
+			path[i]=s;
+		}
+		dis[s]=0;
+		path[s]=-1;
+		//动态规划，最后要得到经过v-1个点到终点的结果
+		for(int i=0;i<v-1;i++){
+			for(int j=0;j<v;j++){
+				//每一次对所有边进行一次松弛，将图向外扩展一层（一个顶点）
+				adjIterator iter(*this,j);
+				for(Edge e=iter.begin();!iter.end();e=iter.next()){
+					if(dis[e.to]>dis[e.from]+e.weight){
+						dis[e.to]=dis[e.from]+e.weight;
+						path[e.to]=e.from;
+					}
+				}
+			}
+		}
+	}
+
+	void SPFA(int s,int *path,double *dis){		
+		//初始化很重要，所有距离最大 ，都不在队列
+		int *inqueue=new int[v];
+		for(int i=0;i<v;i++){
+			inqueue[i]=0;
+			dis[i]=0x3f3f3f3f;
+		}
+		path[0]=-1;
+		dis[s]=0;
+		//把源放入队列
+		queue<int> q;
+		q.push(s);
+		inqueue[s]=1;
+		while(!q.empty()){
+			int u=q.front();
+			q.pop();
+			inqueue[u]=0;
+			//pop出来的点进行松弛，松弛的点放入队列（如果不在的话）
+			adjIterator iter(*this,u);
+			for(Edge e=iter.begin();!iter.end();e=iter.next()){
+				if(dis[e.to]>dis[e.from]+e.weight){
+					dis[e.to]=dis[e.from]+e.weight;
+					path[e.to]=e.from;
+					if(!inqueue[e.to]){
+						q.push(e.to);
+						inqueue[e.to]=1;
+					}
+				}
+			}
+		}
+	}
+	
+
+	void Floyd(int** D,int** P){        //D:D[i][j]表示从i到j的最短距离；P:P[i][j]表示从i到j的最短路径上的结点      
+	    for(int i=0;i<v;i++)
+	        for(int j=0;j<v;j++){    
+	            D[i][j]=0x3f3f3f3f;    
+	            P[i][j]=i;    
+	        }  
+	    for(int i=0;i<v;i++){    
+	        adjIterator iter(*this,i);
+	    	for(Edge e=iter.begin();!iter.end();e=iter.next())
+	    		D[e.from][e.to]=e.weight;
+	    }   
+	    for(int i=0;i<v;i++){    
+	        D[i][i]=0;    
+	        P[i][i]=0;    
+	    }  //注意对角线初始化0  
+	    for(int k=0;k<v;k++)    
+	        for(int i=0;i<v;i++)    
+	            for(int j=0;j<v;j++)    
+	                if(D[i][j]>D[i][k]+D[k][j]){    
+	                    D[i][j]=D[i][k]+D[k][j];    
+	                    P[i][j]=P[k][j];    
+	                }  //比较所有i经过点k然后再到j的距离  
+	}    
+
 	void __DFS(int pos,int visited[]){
 		adjIterator iter(*this,pos);
 		visited[pos]=1;
@@ -112,6 +261,7 @@ public:
 	}
 
 	//迭代器
+
 
 	class adjIterator {		
 	private:
--- test.cpp
+++ test.cpp
@@ -1,21 +1,27 @@
-#include"SplayTree.h"
+#include"SparseGraph.h"
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-	SplayTree<int> tree;
-	bool flag;
-	//tree.root=NULL;
-	for(int i=0;i<5;i++)
-	tree.insert(i);	
-	tree.preOrderTraverse(tree.root);
-	tree.remove(3);
-	//cout<<endl<<tree.find(5)->val;
-	//cout<<"result: "<<tree.root->val<<endl;
-	
+	SparseGraph G(5);
+	G.addedge(0,1,8);
+	G.addedge(1,2,7);
+	G.addedge(2,3,6);
+	G.addedge(3,4,5);
+	G.addedge(2,4,4);
+	G.addedge(0,4,3);
+	G.addedge(1,4,2);
+	G.addedge(0,2,1);
+	//G.show();
+	int path[5];
+	double dis[5];
+	vector<Edge> tree;
+	cout<<G.Prim(0,tree);
 	cout<<endl;
-	tree.search(2);
-	tree.preOrderTraverse(tree.root);
-	tree.search(4);
+	for(int i=0;i<tree.size();i++)
+		cout<<tree[i].from<<" "<<tree[i].to<<" ";
 	cout<<endl;
-	tree.preOrderTraverse(tree.root);
+	G.show();
+	// for(int i=0;i<5;i++)
+	// 	cout<<path[i]<<" ";
 }
