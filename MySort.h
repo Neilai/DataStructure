48c93909ac6d218f73988369cf5f89d1cea16644
Neilai <1131894367@qq.com>
2017-12-25 23:33:16 +0800

    update merge sort algorithms

--- MySort.h
+++ MySort.h
@@ -1,8 +1,9 @@
 #include<ctime>
 #include<cstdlib>
 using namespace std;
-
-void QuickSort3(double arr[],int L,int R){	
+//三路快速排序
+template<typename T>
+void QuickSort3(T arr[],int L,int R){	
 
 	if(R<=L)
 		return;
@@ -36,7 +37,9 @@ void QuickSort3(double arr[],int L,int R){
 }
 
 
-void QuickSort2(double arr[],int L,int R){
+//两路快速排序
+template<typename T>
+void QuickSort2(T arr[],int L,int R){
 
 	if(R<=L)
 		return;
@@ -64,8 +67,9 @@ void QuickSort2(double arr[],int L,int R){
 	QuickSort2(arr,lt,R);
 }
 
-
-void QuickSort1(double arr[],int L,int R){
+//最简单的一路快速排序
+template<typename T>
+void QuickSort1(T arr[],int L,int R){
 
 	if(R<=L)
 		return;
@@ -87,3 +91,59 @@ void QuickSort1(double arr[],int L,int R){
 	QuickSort1(arr,lt,R);
 
 }
+
+template<typename T>
+merge(T arr[],int L,int mid,int R){
+	
+	T *tmp=new T[R-L+1];
+	
+	for(int i=L;i<=R;i++){
+		tmp[i-L]=arr[i];
+	}
+	
+	int i=L;
+	int j=mid+1;
+	int k=L;
+	
+	while(k<=R){
+		if(i>mid){
+			arr[k]=tmp[j-L];
+			j++;
+		}
+		else if(j>R){
+			arr[k]=tmp[i-L];
+			i++;
+		}
+		else if(tmp[i-L]<tmp[j-L]){
+			arr[k]=tmp[i-L];
+			i++;
+		}
+		else{
+			arr[k]=tmp[j-L];
+			j++;
+		}
+		k++;
+	}
+	delete[] tmp;
+
+}
+
+
+template<typename T>
+void mergesort(T arr[],int L,int R){
+	if(L>=R)
+		return;
+	int mid=(L+R)/2;
+	mergesort(arr,L,mid);
+	mergesort(arr,mid+1,R);
+	merge<T>(arr,L,mid,R);
+}
+
+template<typename T>
+void mergesortBU(T arr[],int n){
+	for(int sz=1;sz<n;sz=sz*2)
+		for(int i=0;i<n-sz;i+=sz*2)
+			merge<T>(arr,i,i+sz-1,min(n,i+sz+sz-1));
+}
+
+
